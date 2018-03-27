e10e678a74ec970769d97e19284152b16aef0fe2
Peter Kim <kroits09@gmail.com>
2018-03-27 18:35:16 +0900

    T-Shirt Info” section of the form Updated
    For the T-Shirt color menu, only display the color options that match the design selected in the "Design" menu.
    If the user selects "Theme - JS Puns" then the color menu should only display "Cornflower Blue," "Dark Slate Grey," and "Gold."
    If the user selects "Theme - I ♥ JS" then the color menu should only display "Tomato," "Steel Blue," and "Dim Grey."

--- js/app.js
+++ js/app.js
@@ -18,3 +18,33 @@ jobRole.addEventListener('input', (e) => {
   }
   otherJobTitle.focus();
 });
+
+// For the T-Shirt color menu, only display the color options that match the design selected in the "Design" menu.
+  // Hide T-Shirt color div
+  tshirtColorsDiv.style.display = 'none';
+  // If the user selects "Theme - JS Puns" then the color menu should only display "Cornflower Blue," "Dark Slate Grey," and "Gold."
+  // If the user selects "Theme - I ♥ JS" then the color menu should only display "Tomato," "Steel Blue," and "Dim Grey."
+//
+tshirtTheme.addEventListener('change', (e) => {
+  if (e.target.value === 'js puns') {
+    tshirtColorsDiv.style.display = '';
+    tshirtColors.selectedIndex = 0;
+    tshirtColors[0].style.display = '';
+    tshirtColors[1].style.display = '';
+    tshirtColors[2].style.display = '';
+    tshirtColors[3].style.display = 'none';
+    tshirtColors[4].style.display = 'none';
+    tshirtColors[5].style.display = 'none';
+  } else if (e.target.value === 'heart js') {
+    tshirtColorsDiv.style.display = '';
+    tshirtColors.selectedIndex = 3;
+    tshirtColors[0].style.display = 'none';
+    tshirtColors[1].style.display = 'none';
+    tshirtColors[2].style.display = 'none';
+    tshirtColors[3].style.display = '';
+    tshirtColors[4].style.display = '';
+    tshirtColors[5].style.display = '';
+  } else {
+    tshirtColorsDiv.style.display = 'none';
+  }
+});
