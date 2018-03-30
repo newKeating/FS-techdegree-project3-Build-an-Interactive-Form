b99b6266aeb8e75f29376813e81cbd7a1ef3682b
Peter Kim <kroits09@gmail.com>
2018-03-30 22:45:31 +0900

    Total Cost printing added
    As a user selects activities, a running total should display below the list of checkboxes. For example, if the user selects "Main Conference", then Total: $200 should appear. If they add 1 workshop, the total should change to Total: $300.

--- js/app.js
+++ js/app.js
@@ -4,6 +4,11 @@ const jobRole = document.querySelector('#title');
 const tshirtTheme = document.querySelector('#design');
 const tshirtColorsDiv = document.querySelector('#colors-js-puns');
 const tshirtColors = document.querySelector('#color');
+const activities = document.querySelector('.activities');
+const activitiesCheckboxes = document.querySelectorAll('fieldset.activities > label > input');
+const p = document.createElement('p');
+let totalCost = 0;
+activities.appendChild(p);
 
 // Set focus on the first text field on page-load
 nameField.focus();
@@ -48,3 +53,91 @@ tshirtTheme.addEventListener('change', (e) => {
     tshirtColorsDiv.style.display = 'none';
   }
 });
+
+// ”Register for Activities” section of the form
+  // Disable checkboxes for workshops that are conflicting with user's selections.
+activities.addEventListener('change', (e) => {
+  toggleCheckbox(e);
+  printTotal(e);
+});
+
+const toggleCheckbox = (e) => {
+  const isChecked = e.target.checked;
+  const checkboxName = e.target.name;
+  const disableCheckboxes = (num1, num2, boo) => {
+    const label = document.querySelectorAll('fieldset.activities > label');
+    activitiesCheckboxes[num1].disabled = boo;
+    activitiesCheckboxes[num2].disabled = boo;
+    // disable class adds grey color
+    if (boo) {
+      label[num1].className = 'disable';
+      label[num2].className = 'disable';
+    } else {
+      label[num1].classList.remove('disable');
+      label[num2].classList.remove('disable');
+    }
+  }
+
+  if(isChecked) {
+    switch(checkboxName) {
+      case 'js-frameworks':
+        disableCheckboxes(3, 5, true);
+        break;
+      case 'js-libs':
+        disableCheckboxes(4, 6, true);
+        break;
+      case 'express':
+        disableCheckboxes(1, 5, true);
+        break;
+      case 'node':
+        disableCheckboxes(2, 6, true);
+        break;
+      case 'build-tools':
+        disableCheckboxes(1, 3, true);
+        break;
+      case 'npm':
+        disableCheckboxes(2, 4, true);
+        break;
+    }
+  } else if(!isChecked) {
+    switch(checkboxName) {
+      case 'js-frameworks':
+        disableCheckboxes(3, 5, false);
+        break;
+      case 'js-libs':
+        disableCheckboxes(4, 6, false);
+        break;
+      case 'express':
+        disableCheckboxes(1, 5, false);
+        break;
+      case 'node':
+        disableCheckboxes(2, 6, false);
+        break;
+      case 'build-tools':
+        disableCheckboxes(1, 3, false);
+        break;
+      case 'npm':
+        disableCheckboxes(2, 4, false);
+        break;
+    }
+  }
+}
+
+const printTotal = (e) => {
+  const isChecked = e.target.checked;
+  const calculateTotal = (e) => {
+    if(isChecked) {
+      totalCost += parseInt(e.target.value);
+    } else if(!isChecked) {
+      totalCost -= parseInt(e.target.value);
+    }
+    return totalCost;
+  }
+  calculateTotal(e);
+  p.textContent = `Total: $${totalCost}`;
+  if(totalCost === 0) {
+    p.style.display = 'none';
+  } else {
+    p.style.display = '';
+  }
+}
