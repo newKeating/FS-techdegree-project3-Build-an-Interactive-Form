551f5f3899ea05ce2671f738387f4c963d7e72e3
Peter Kim <kroits09@gmail.com>
2018-04-02 19:13:39 +0900

    stuck at validation
    stuck

--- js/app.js
+++ js/app.js
@@ -1,4 +1,3 @@
-const nameField = document.querySelector('#name');
 const otherJobTitle = document.querySelector('#other-title');
 const jobRole = document.querySelector('#title');
 const tshirtTheme = document.querySelector('#design');
@@ -14,8 +13,19 @@ const $paymentOptions = $('#payment');
 const $creditcardDiv = $('#credit-card');
 const $paypalDiv = $('#paypal');
 const $bitcoinDiv = $('#bitcoin');
-// Set focus on the first text field on page-load
-nameField.focus();
+const nameInput = document.querySelector('#name');
+const emailInput = document.querySelector('#mail');
+const cardNumInput = document.querySelector('#cc-num');
+const zipcodeInput = document.querySelector('#zip');
+const cvvInput = document.querySelector('#cvv');
+const form = document.querySelector('form');
+const $errors = $('.error');
+$errors.hide();
+const $nameError = $('#name-error');
+const $emailError = $('#email-error-wrongType');
+const $ccNumError = $('#cc-error');
+const $zipError = $('#zip-error');
+const $cvvError = $('#cvv-error');
 
 // A text field that will be revealed when the "Other" option is selected from the "Job Role" drop down menu.
 otherJobTitle.style.display = 'none';
@@ -68,60 +78,45 @@ activities.addEventListener('change', (e) => {
 const toggleCheckbox = (e) => {
   const isChecked = e.target.checked;
   const checkboxName = e.target.name;
-  const disableCheckboxes = (num1, num2, boo) => {
+  const disableCheckboxes = (num, boo) => {
     const label = document.querySelectorAll('fieldset.activities > label');
-    activitiesCheckboxes[num1].disabled = boo;
-    activitiesCheckboxes[num2].disabled = boo;
+    activitiesCheckboxes[num].disabled = boo;
     // disable class adds grey color
     if (boo) {
-      label[num1].className = 'disable';
-      label[num2].className = 'disable';
+      label[num].className = 'disable';
     } else {
-      label[num1].classList.remove('disable');
-      label[num2].classList.remove('disable');
+      label[num].classList.remove('disable');
     }
   }
 
   if(isChecked) {
     switch(checkboxName) {
       case 'js-frameworks':
-        disableCheckboxes(3, 5, true);
+        disableCheckboxes(3, true);
         break;
       case 'js-libs':
-        disableCheckboxes(4, 6, true);
+        disableCheckboxes(4, true);
         break;
       case 'express':
-        disableCheckboxes(1, 5, true);
+        disableCheckboxes(1, true);
         break;
       case 'node':
-        disableCheckboxes(2, 6, true);
-        break;
-      case 'build-tools':
-        disableCheckboxes(1, 3, true);
-        break;
-      case 'npm':
-        disableCheckboxes(2, 4, true);
+        disableCheckboxes(2, true);
         break;
     }
   } else if(!isChecked) {
     switch(checkboxName) {
       case 'js-frameworks':
-        disableCheckboxes(3, 5, false);
+        disableCheckboxes(3, false);
         break;
       case 'js-libs':
-        disableCheckboxes(4, 6, false);
+        disableCheckboxes(4, false);
         break;
       case 'express':
-        disableCheckboxes(1, 5, false);
+        disableCheckboxes(1, false);
         break;
       case 'node':
-        disableCheckboxes(2, 6, false);
-        break;
-      case 'build-tools':
-        disableCheckboxes(1, 3, false);
-        break;
-      case 'npm':
-        disableCheckboxes(2, 4, false);
+        disableCheckboxes(2, false);
         break;
     }
   }
@@ -171,5 +166,56 @@ $paymentOptions.on('change', (e)=>{
     $creditcardDiv.hide();
     $paypalDiv.hide();
     $bitcoinDiv.show();
+  } else {
+    $creditcardDiv.hide();
+    $paypalDiv.hide();
+    $bitcoinDiv.hide();
+  }
+});
+// Form validation - If any of the following validation errors exist, prevent the user from submitting the form:
+  // 1. Name field can't be blank
+  // 2. Email field must be a validly formatted e-mail address (you don't have to check that it's a real e-mail address, just that it's formatted like one: dave@teamtreehouse.com for example.
+  // 3. Must select at least one checkbox under the "Register for Activities" section of the form.
+  // 4. If the selected payment option is "Credit Card," make sure the user has supplied a credit card number, a zip code, and a 3 number CVV value before the form can be submitted.
+    // - Credit card field should only accept a number between 13 and 16 digits
+    // - The zipcode field should accept a 5-digit number
+    // - The CVV should only accept a number that is exactly 3 digits long
+
+// if any validator returns false it's not valid
+  // validator functions
+    // name field validator
+
+// if validator functions return false
+  // show error message function
+  // error style function
+const nameValidator = () => {
+  nameInput.addEventListener('input', (e) => {
+    return /\S+/.test(e.target.value)
+  });
+}
+
+
+let nameValid = /\S+/.test(nameInput.value);
+let emailValid = /^[^@]+@[^@.]+\.[a-z]+$/i.test(emailInput.value);
+let checkboxValid = () => {
+  for(let i=0; i<activitiesCheckboxes.length; i++) {
+    if(activitiesCheckboxes[i].checked) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
+let ccNumValid = /^\d{13,16}$/.test(cardNumInput.value);
+let zipValid = /^\d{5}$/.test(zipcodeInput.value);
+let cvvValid = /^\d{3}$/.test(cvvInput.value);
+
+form.addEventListener('submit', (e) => {
+  if(!nameValidator()) {
+    e.preventDefault();
+    $nameError.show();
+    nameInput.focus();
+  } else if(nameValidator()) {
+    $nameError.hide();
   }
 });
